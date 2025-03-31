996de9664e18fa192af5847192d33f5d9243fa56
zeinab-sheikhi <zeinab.sheykhie@gmail.com>
2025-03-31 19:49:54 +0200

    causal attention unfinished

--- train_gpt2.py
+++ train_gpt2.py
@@ -12,6 +12,55 @@ class GPTConfig:
     n_head: int = 6
     n_embd: int = 384
 
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, value, query projections for all heads, but in a batch 
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_mebd * 3, config.n_embd)
+        # regularization 
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # not really a bias, more of a mask, but following the OpenAI-HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence lenght, embedding dimention
+        
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
 class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
